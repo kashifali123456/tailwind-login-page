ba5dcd636a72a7872f2eec77ba61d9d4779a79a9
kashifali123456 <89785049+kashifali123456@users.noreply.github.com>
2023-09-19 19:44:35 +0500

    final code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,10 @@ module.exports = {
     extend: {
       colors: {
         "black-dark": "#00000050",
+        "dull-white": "#ffffffb3",
+        "white-light": "#ffffff30",
+        "white-medium": "#ffffff40",
+        "neon-blue": "#2fb8ff",
       },
     },
   },
